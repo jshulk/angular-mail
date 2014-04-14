1cb2cce8c9cef003c2b38e4c3956c008995e21ef
vjunloc <vjunloc@vm-0.vjunloc.koding.kd.io>
2014-04-14 08:59:06 +0000

    Finished the compose window

--- assets/js/mail/controllers/ComposeController.js
+++ assets/js/mail/controllers/ComposeController.js
@@ -1,5 +1,16 @@
 define(["mail"], function(app){
 	return app.controller('ComposeController', function($scope){
 		$scope.createNew = true;
+		$scope.$emit("toggle:content");
+		console.log('instantiated compose controller');
+
+		$scope.$on("$stateChangeStart", function(event, toState){
+			console.log('stae change start in compose controller');
+			console.log(toState);
+			if( toState.name == "mail.type.compose") {
+				$scope.$emit("toggle:content");
+			}
+		});
+
 	});
 });
